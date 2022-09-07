40a1db6ede4a611fab6227056275744d78902105
Italo Pessoa <sr.italopessoa@gmail.com>
2022-09-07 16:47:15 -0400

    variáveis com nome dos candidados renomeadas.

--- R/TesteChiQuadradoSecoes.R
+++ R/TesteChiQuadradoSecoes.R
@@ -1,8 +1,6 @@
 library(tidyverse)
 #library(ggstatsplot)
 
-# setwd("~/LaboratorioCienciaDados/votacao_secao_2018_RO")
-setwd("./AnaliseVotacao2014x2018")
 
 dados <- read.csv2('bweb_1t_RO_101020182022.csv')
 
@@ -12,13 +10,13 @@ dados <-  filter(dados, DS_CARGO_PERGUNTA == "Presidente")
 zona = 6
 cod_mun = 35
 cod_local = 1040
-num_bolsonaro = 17
-num_haddad = 13
+num_cand_PSL = 17
+num_cand_PT = 13
 
 
 
 dados_zona <- filter(dados, NR_ZONA == zona) %>%
-  select(CD_MUNICIPIO, CD_MUNICIPIO, NR_ZONA, NR_SECAO, NR_LOCAL_VOTACAO,
+  select(CD_MUNICIPIO, NR_ZONA, NR_SECAO, NR_LOCAL_VOTACAO,
          QT_COMPARECIMENTO, NR_VOTAVEL, NM_VOTAVEL, QT_VOTOS)
 
 dados_local <- filter(dados_zona, NR_ZONA == zona, CD_MUNICIPIO == cod_mun,
@@ -47,11 +45,11 @@ for (i in 1:n_secoes) {
     summarise(QT_VOTOS = sum(QT_VOTOS))
   nulos_brancos <- as.data.frame(nulos_brancos)
 
-  bolsonaro <- filter(dados_secao, NR_VOTAVEL == num_bolsonaro)
+  cand_PSL <- filter(dados_secao, NR_VOTAVEL == num_cand_PSL)
 
-  haddad <- filter(dados_secao, NR_VOTAVEL == num_haddad)
+  cand_PT <- filter(dados_secao, NR_VOTAVEL == num_cand_PT)
 
-  demais_cand <- filter(dados_secao, !(NR_VOTAVEL == num_haddad |NR_VOTAVEL == num_bolsonaro |
+  demais_cand <- filter(dados_secao, !(NR_VOTAVEL == num_cand_PT |NR_VOTAVEL == num_cand_PSL |
                                          NR_VOTAVEL == 95 | NR_VOTAVEL == 96)) %>%
     group_by(CD_MUNICIPIO, NR_ZONA, NR_SECAO, NR_LOCAL_VOTACAO,
              QT_COMPARECIMENTO, NR_VOTAVEL = 00, NM_VOTAVEL = "Demais Candidatos") %>%
@@ -59,9 +57,9 @@ for (i in 1:n_secoes) {
 
   demais_cand <- as.data.frame(demais_cand)
 
-  votacao_local <- rbind(votacao_local, nulos_brancos, demais_cand, bolsonaro, haddad)
+  votacao_local <- rbind(votacao_local, nulos_brancos, demais_cand, cand_PSL, cand_PT)
 
-  qt_votos_secao <- rbind(nulos_brancos, demais_cand, bolsonaro, haddad) %>%
+  qt_votos_secao <- rbind(nulos_brancos, demais_cand, cand_PSL, cand_PT) %>%
     select(NM_VOTAVEL, QT_VOTOS)
 
   qt_votos_secao <- qt_votos_secao %>% pivot_wider(names_from = NM_VOTAVEL,
@@ -84,9 +82,8 @@ matriz_votacao <- select(matriz_votacao, -`Nulos e Brancos`, -`Demais Candidatos
 
 #mat_alunos <- rbind(c(8,210,72,10,75,125),c(20,180,55,10,90,145))
 
-test <- chisq.test(matriz_votacao)
+chisq.test(matriz_votacao)
 
-test
 
 ## plot with statistical results
 #ggpiestats(
